b0c8679970af5f785ea4543d433201dc9da71b66
Daniel Foster <danieljfoster87@gmail.com>
2018-05-06 19:50:10 -0400

    styling page and making calls to db

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+-- 3. Still in the `db` folder, create a `seeds.sql` file. In this file, write insert queries to populate the `burgers` table with at least three entries.
+
+INSERT INTO burgers (burger_name)
+VALUES ("Krabby Patty");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Chipotle Bacon");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Burgernut");
